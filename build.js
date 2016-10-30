df74cd0adf171a936b07e1093169e59cbe39ccf3
Harri Siirak <harri.siirak@gmail.com>
2016-10-30 18:24:44 +0200

    Fix missing template data

--- build.js
+++ build.js
@@ -11,7 +11,7 @@ function generateDocument (products, template = 'default', path = './build/') {
       updated: moment().tz('Europe/Tallinn').format('YYYY-MM-DD HH:mm:ss')
     };
 
-    ejs.renderFile('./templates/' + template + '.ejs', data, { }, (err, str) => {
+    ejs.renderFile('./templates/' + template + '.ejs', data, data, (err, str) => {
       if (err) {
         reject(err);
         return;
